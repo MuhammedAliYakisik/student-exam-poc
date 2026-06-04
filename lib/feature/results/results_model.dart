fdb9b84ddca0936b765eed54aa01a7764660152c
Muhammed Ali <muhammedali8799@gmail.com>
2026-06-04 13:50:01 +0300

    feat: implement result class Model

---
+++ lib/feature/results/results_model.dart
@@ -0,0 +1,31 @@
+class ResultsModel {
+  final int id;
+  final int studentId;
+  final int courseId;
+  final double score;
+
+  ResultsModel({
+    required this.id,
+    required this.studentId,
+    required this.courseId,
+    required this.score,
+  });
+
+  factory ResultsModel.fromMap(Map<String, dynamic> map) {
+    return ResultsModel(
+      id: map['id'] ?? 0,
+      studentId: map['student_id'] ?? 0,
+      courseId: map['course_id'] ?? 0,
+      score: map['score'] ?? 0.0,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'student_id': studentId,
+      'course_id': courseId,
+      'score': score,
+    };
+  }
+
+}
